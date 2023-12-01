e2c146c1086482e57b14e0e86fa3a119f66fcea0
MOHD IZZAT BIN AMRAN <bcs22020001@student.uts.edu.my>
2023-12-01 16:47:42 +0800

    Add project files.

---
+++ LabTest/LabTest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LabTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Frame circleImageFrame = new Frame
+            {
+                Margin = 10,
+                BorderColor = Color.Orange,
+                CornerRadius = 50,
+                HeightRequest = 60,
+                WidthRequest = 60,
+                IsClippedToBounds = true,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Content = new Image
+                {
+                    Source = ImageSource.FromFile("image5.jpg"),
+                    Aspect = Aspect.AspectFill,
+                    Margin = 10,
+                    HeightRequest = 100,
+                    WidthRequest = 100
+                }
+            };
+        }
+    }
+            
+}
